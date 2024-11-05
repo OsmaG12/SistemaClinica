ec52c64bcc8fe37980a4105d0b833c22c3982c34
miche <roxana.valencia@catolica.edu.sv>
2024-11-04 21:04:18 -0600

    Se agregaron los controlodares de Administrador, Doctores y Enfermeros con sus respectivas Vistas

---
+++ SistemaClinica/Controllers/EnfermerosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaClinica.Controllers
+{
+    public class EnfermerosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
